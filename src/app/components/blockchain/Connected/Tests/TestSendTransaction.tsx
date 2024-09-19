f1a9a1c20d39b6ddfd1d62d441241d11b38686f4
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2024-09-19 13:24:53 -0300

    initial commit

---
+++ src/app/components/blockchain/Connected/Tests/TestSendTransaction.tsx
@@ -0,0 +1,70 @@
+import { Box, Text } from "@0xsequence/design-system";
+import React, { useEffect, useState } from "react";
+import { Chain } from "viem";
+import { useAccount, useSendTransaction, useWalletClient } from "wagmi";
+import { chains } from "../../../../../app/Providers";
+import CardButton from "../../../CardButton";
+import ErrorToast from "../../../ErrorToast";
+
+const TestSendTransaction = () => {
+  const { data: walletClient } = useWalletClient();
+  const { chainId } = useAccount();
+  const [network, setNetwork] = useState<Chain | null>(null);
+  const {
+    data: txnData,
+    sendTransaction,
+    isPending: isPendingSendTxn,
+    error,
+    reset,
+  } = useSendTransaction();
+  const [lastTransaction, setLastTransaction] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (txnData) {
+      setLastTransaction(txnData);
+    }
+    if (error) console.error(error);
+  }, [txnData, error]);
+
+  useEffect(() => {
+    if (!chainId) return;
+    const chainResult = chains.find((chain) => chain.id === chainId);
+    if (chainResult) {
+      setNetwork(chainResult);
+    }
+  }, [chainId]);
+
+  const runSendTransaction = async () => {
+    const [account] = await walletClient!.getAddresses();
+    sendTransaction({ to: account, value: BigInt(0), gas: null });
+  };
+
+  return (
+    <>
+      <CardButton
+        title="Send transaction"
+        description="Send a transaction with your wallet"
+        isPending={isPendingSendTxn}
+        onClick={runSendTransaction}
+      />
+      {lastTransaction && (
+        <Box display="flex" flexDirection="column" gap="4">
+          <Text>Last transaction hash: {lastTransaction}</Text>
+          <button>
+            <a
+              target="_blank"
+              href={`${network?.blockExplorers?.default?.url}/tx/${lastTransaction}`}
+            >
+              Click to view on {network?.name}
+            </a>
+          </button>
+        </Box>
+      )}
+      {error && (
+        <ErrorToast message={error?.message} onClose={reset} duration={7000} />
+      )}
+    </>
+  );
+};
+
+export default TestSendTransaction;
---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Text } from "@0xsequence/design-system";
+import { useAccount } from "wagmi";
+import Connected from "./components/blockchain/Connected";
+import NotConnected from "./components/blockchain/NotConnected";
+
+const HomePage = () => {
+  const { isConnected } = useAccount();
+  return (
+    <div>
+      <h1>Sequence Kit Starter - Nextjs</h1>
+      <h2 className="homepage__marginBtNormal">Embedded Wallet</h2>
+      {isConnected ? <Connected /> : <NotConnected />}
+      <footer className="homepage__footer">
+        <Text>
+          Want to learn more? Read the{" "}
+          <a
+            href={
+              "https://docs.sequence.xyz/solutions/wallets/sequence-kit/overview/"
+            }
+            target="_blank"
+            rel="noreferrer "
+          >
+            docs
+          </a>
+          !
+        </Text>
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
