fdbed3e56b226f39a99d4744cc879e3bd718ec97
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2024-09-26 00:17:45 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-//import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    //mkcert({ hosts: ['localhost.direct'] }),
-    react(),
-    vanillaExtractPlugin(),
-  ],
-  // For github pages https://0xsequence.github.io/demo-waas-auth/
-  base: mode === 'production' ? '/demo-waas-auth/' : '/',
-}))
+export default defineConfig({
+  base: './',  // Asegura que el valor sea el correcto
+  build: {
+    outDir: 'dist',
+  },
+});
+
