f5a92665e6a456c4258f7cce5ed60aee0294cd11
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2024-09-26 00:22:26 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
-export default defineConfig({
-  base: './',  // Asegura que el valor sea el correcto
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+  ],
+  base: './',
   build: {
     outDir: 'dist',
   },
-});
+}))
 
